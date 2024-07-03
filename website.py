763bcc8ebe063b194e83ec4e126ff294f67a1456
Moon YuBin <135791086+YUBIN-githubb@users.noreply.github.com>
2024-07-03 09:43:57 +0000

    웹사이트 request 보내기까지

--- website.py
+++ website.py
@@ -1,6 +1,27 @@
+from requests import get
+#for loop 연습하기
+#pypi 연습하기 - requests 모듈 
+
 websites = (
   'google.com',
   'airbnb.com',
-  'twitter.com',
-  'facebook.com'
+  'https://twitter.com',
+  'facebook.com',
+  'https://tiktok.com'
 )
+
+results = {
+  
+}
+
+#200 response 값을 웹사이트가 성공적으로 응답했다는 의미
+for site in websites:
+  if not site.startswith('https://'):
+    site = f"https://{site}"
+  response = get(site)
+  if response.status_code >= 100 and response.status_code < 400:
+    results[site] = "OK"
+  else:
+    results[site] = "FAILED"
+
+print(results)
